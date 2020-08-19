0a4b1c797691f47416e74c10d17f51487802578f
Daniel Burgess <dnl@tuta.io>
2020-08-19 22:26:50 +0100

    Check that the society exists on the welcome page

--- lightbluetent/society.py
+++ lightbluetent/society.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, abort
 from lightbluetent.models import db, Society
 
 bp = Blueprint("society", __name__, url_prefix="/s")
@@ -7,5 +7,8 @@ bp = Blueprint("society", __name__, url_prefix="/s")
 def welcome(uid):
 
     society = Society.query.filter_by(uid=uid).first()
+
+    if not society:
+        return abort(404)
 
     return render_template("society/welcome.html", page_title=f"Home page for { society.name }", society=society)
