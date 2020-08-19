0f3bc6a0e55a855305f0097072bdead8bbb01b22
Daniel Burgess <dnl@tuta.io>
2020-08-19 14:34:51 +0100

    Separate the user and society sign-up pages, add many-to-many relationship

--- lightbluetent/home.py
+++ lightbluetent/home.py
@@ -26,6 +26,68 @@ def logout():
     flash("You have been logged out.")
     return redirect(url_for("home.index"))
 
+@bp.route("/register_soc", methods=("GET", "POST"))
+@auth_decorator
+def register_soc():
+    crsid = auth_decorator.principal
+
+    user = User.query.filter_by(crsid=crsid).first()
+
+    # Check the user is registered with us, if not redirect to the user reg page
+    if not user:
+        return redirect(url_for("home.register"))
+
+    if request.method == "POST":
+
+        values = {}
+        for key in ("soc_name", "soc_short_name"):
+            values[key] = request.form.get(key, "").strip()
+
+        errors = {}
+
+        values["uid"] = values["soc_short_name"].lower()
+        values["bbb_id"] = gen_unique_string()
+        values["moderator_pw"] = gen_unique_string()[0:12]
+        values["attendee_pw"] = gen_unique_string()[0:12]
+
+        # TODO: What's the best way of handling the (unlikely) event
+        #       that the passwords are:
+        #    a) non-unique across registered societies
+        #    b) the same
+        #       Currently we abort(500)
+        if values["moderator_pw"] == values["attendee_pw"]:
+            abort(500)
+        elif (Society.query.filter_by(attendee_pw=values["attendee_pw"]).first()
+                or Society.query.filter_by(moderator_pw=values["moderator_pw"]).first()
+                or Society.query.filter_by(bbb_id=values["bbb_id"]).first()):
+            abort(500)
+
+        if Society.query.filter_by(uid=values["uid"]).first():
+            errors["soc_short_name"] = "That society short name is already in use."
+
+        if errors:
+            return render_template("home/register_soc.html", page_title="Register a society", crsid=crsid, errors=errors, **values)
+        else:
+            society = Society(short_name=values["soc_short_name"],
+                              name=values["soc_name"],
+                              attendee_pw=values["attendee_pw"],
+                              moderator_pw=values["moderator_pw"],
+                              uid=values["uid"],
+                              bbb_id=values["bbb_id"])
+
+            db.session.add(society)
+            db.session.commit()
+
+            user.societies.append(society)
+            db.session.commit()
+
+            return redirect(url_for("home.register_soc_success"))
+
+
+    else:
+        return render_template("home/register_soc.html", page_title="Register a society", crsid=crsid, errors={})
+
+
 @bp.route("/register", methods=("GET", "POST"))
 @auth_decorator
 def register():
@@ -37,7 +99,7 @@ def register():
         # Input validation from https://github.com/SRCF/control-panel/blob/master/control/webapp/signup.py#L37
 
         values = {}
-        for key in ("first_name", "surname", "email_address", "soc_name", "soc_short_name"):
+        for key in ("first_name", "surname", "email_address"):
             values[key] = request.form.get(key, "").strip()
 
         for key in ("dpa", "tos"):
@@ -59,50 +121,23 @@ def register():
         if not values["tos"]:
             errors["tos"] = "You must accept the terms of service to register."
 
-        values["uid"] = values["soc_short_name"].lower()
-        values["bbb_id"] = gen_unique_string()
-        values["moderator_pw"] = gen_unique_string()[0:12]
-        values["attendee_pw"] = gen_unique_string()[0:12]
-
         if User.query.filter_by(email=values["email_address"]).first():
             errors["email_address"] = "That email address is already registered."
-        if Society.query.filter_by(uid=values["uid"]).first():
-            errors["soc_short_name"] = "That society short name is already in use."
 
-        # TODO: What's the best way of handling the (unlikely) event
-        #       that the passwords are:
-        #    a) non-unique across registered societies
-        #    b) the same
-        #       Currently we abort(500)
-
-        if values["moderator_pw"] == values["attendee_pw"]:
-            abort(500)
-
-        elif (Society.query.filter_by(attendee_pw=values["attendee_pw"]).first()
-                or Society.query.filter_by(moderator_pw=values["moderator_pw"]).first()
-                or Society.query.filter_by(bbb_id=values["bbb_id"]).first()):
-            abort(500)
+        existing_user = User.query.filter_by(crsid=crsid).first()
+        if existing_user:
+            errors["crsid"] = "This CRSid is already associated with an account."
 
         if errors:
-            return render_template("home/register.html", page_title="Register a society", errors=errors, **values)
+            return render_template("home/register.html", page_title="Register", crsid=crsid, errors=errors, **values)
         else:
-            society = Society(short_name=values["soc_short_name"],
-                              name=values["soc_name"],
-                              attendee_pw=values["attendee_pw"],
-                              moderator_pw=values["moderator_pw"],
-                              uid=values["uid"],
-                              bbb_id=values["bbb_id"])
 
-            db.session.add(society)
-            db.session.commit()
-
-            admin = User(email=values["email_address"],
+            user = User(email=values["email_address"],
                          first_name=values["first_name"],
                          surname=values["surname"],
-                         society_id=society.id,
                          crsid=auth_decorator.principal)
 
-            db.session.add(admin)
+            db.session.add(user)
             db.session.commit()
 
             return redirect(url_for("home.register_success"))
@@ -117,9 +152,13 @@ def register():
             "tos": False
         }
 
-        return render_template("home/register.html", page_title="Register a Society",
+        return render_template("home/register.html", page_title="Register",
                            crsid=crsid, errors={}, **values)
 
 @bp.route("/register/success")
 def register_success():
     return render_template("home/register_success.html", page_title="Success!")
+
+@bp.route("/register_soc/success")
+def register_soc_success():
+    return render_template("/home/register_soc_success.html", page_title="Success!")
