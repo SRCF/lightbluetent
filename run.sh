865e5906dccf9fa62de76b346f64381f457e461e
Matias Silva <matita.martins@gmail.com>
2020-08-20 21:20:35 +0100

    More tweaks

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
-exec gunicorn -w 2 -b unix:/home/mw781/lightbluetent/web.sock \
+exec gunicorn --forwarded-allow-ips "*" --proxy-allow-from "*" -w 2  \
+    -b unix:/home/mw781/lightbluetent/web.sock \
     --log-file - --log-level debug wsgi:app
