3bddb05f3b52d47c1632c933283946000df56762
Matias Silva <mw781@srcf.net>
2020-08-21 00:28:18 +0100

    Reduce log verbosity

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 PIPENV_DONT_LOAD_ENV=1 /home/mw781/.local/bin/pipenv run gunicorn -w 2  \
     -b unix:/public/home/mw781/web.sock \
-    --log-file production.log --log-level debug wsgi:app
+    --log-file production.log wsgi:app
