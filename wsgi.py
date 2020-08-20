7526730bc183277f38b3e9ebb04733c28f6085cd
Matias Silva <mw781@srcf.net>
2020-08-20 18:55:38 +0100

    Make further changes to prod setup

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 from lightbluetent.app import create_app
 import os
 
-app_env = os.getenv("APPLICATION_ENV", "production")
+app_env = os.getenv("APPLICATION_CONFIG", "production")
 if app_env == "production":
     load_dotenv('.env')
 
