eb46bc2fcd61bfdde6c201eae64d4d6f07483fae
Matias Silva <mw781@srcf.net>
2020-09-15 19:15:14 +0100

    Use correct syntax for dictionaries

--- lightbluetent/utils.py
+++ lightbluetent/utils.py
@@ -8,7 +8,7 @@ def gen_unique_string():
 
 def fetch_lookup_data(crsid):
     url = f"https://www.lookup.cam.ac.uk/api/v1/person/crsid/{crsid}"
-    res = requests.get(url, params={'fetch', "email,departingEmail"}, timeout=(0.5, 10))
+    res = requests.get(url, params={"fetch": "email,departingEmail", "format": "json"}, timeout=(0.5, 10))
     if res.status_code == 200:
         # request successful
         return res.json()["result"]["person"]
