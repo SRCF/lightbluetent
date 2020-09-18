e91ed72e6d2ff34242e623540cf284a043c5d1d6
Matias Silva <mw781@srcf.net>
2020-09-18 19:26:59 +0100

    Add better error pages

--- lightbluetent/config.py
+++ lightbluetent/config.py
@@ -18,7 +18,7 @@ class Config(object):
     # Requests up to 1 MB
     MAX_CONTENT_LENGTH = 1024 * 1024
 
-    HAS_DIRECTORY_PAGE = True
+    HAS_DIRECTORY_PAGE = False
     NUMBER_OF_DAYS = 2
 
     DEFAULT_LOGO = "default_logo.png"
