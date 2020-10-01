5008ba90e1ee3c03de75259216d3e6e7393f274b
Matias Silva <mw781@srcf.net>
2020-10-01 18:49:17 +0100

    Improve room styling 1/2

--- lightbluetent/rooms.py
+++ lightbluetent/rooms.py
@@ -79,8 +79,117 @@ def begin(room_id):
         url = meeting.moderator_url(full_name)
         return redirect(url)
 
+@bp.route("/<room_id>/update/<update_type>", methods=["POST"])
+@auth_decorator
+def update(room_id, update_type):
+    # first pass: is the room URL valid?
+    room = Room.query.filter_by(id=room_id).first()
+    if not room:
+        abort(404)
 
-@bp.route("/<room_id>/manage", methods=("GET", "POST"))
+    # second pass: does the room belong to the user?
+    # or does it belong to a user's group?
+    crsid = auth_decorator.principal
+    user = User.query.filter_by(crsid=crsid).first()
+    if room.group_id:
+        group_id = room.group_id
+        group = Group.query.filter_by(id=group_id).first()
+        if group not in user.groups:
+            abort(403)
+    else:
+        if room not in user.rooms:
+            abort(403)
+
+    values = {}
+    errors = {}
+
+    keys = (
+        "name",
+        "welcome_text",
+        "banner_text",
+        "banner_color",
+        "authentication",
+        "password",
+        "whitelist",
+        "alias"
+        )
+
+    values = get_form_values(request, keys)
+
+    for key in ("mute_on_start", "disable_private_chat", "alias_checked"):
+        values[key] = bool(request.form.get(key, False))
+
+    if len(values["welcome_text"]) > 500:
+        errors["welcome_text"] = "Welcome text is too long."
+    if len(values["banner_text"]) > 200:
+        errors["banner_text"] = "Banner text is too long."
+    if len(values["whitelist"]) > 7:
+        errors["whitelist"] = "Invalid CRSid."
+
+    if values["alias_checked"]:
+        if values["alias"] == "":
+            errors["alias"] = "You must specify the name of your customised page."
+        elif not validate_room_alias(values["alias"]):
+            errors["alias"] = "Invalid name."
+
+        room_with_alias = Room.query.filter_by(alias=values["alias"]).first()
+
+        if room_with_alias and room_with_alias.id != room.id:
+            errors["alias"] = "That URL is already in use. Choose a different one."
+
+    if not errors:
+        room.name = values["name"]
+        room.welcome_text = values["welcome_text"] if values["welcome_text"] != "" else None
+        room.banner_text = values["banner_text"] if values["banner_text"] != "" else None
+        room.banner_color = values["banner_color"]
+        room.authentication = Authentication(values["authentication"])
+        room.alias = values["alias"] if values["alias"] != "" else None
+        room.mute_on_start = values["mute_on_start"]
+        room.disable_private_chat = values["disable_private_chat"]
+        room.updated_at = datetime.now()
+
+        if values["whitelist"] != "":
+            current_app.logger.info(
+                f"{ crsid } is whitelisting '{ values['whitelist'] }' for room '{ room.id }'..."
+            )
+
+            # Whitelist a new CRSid.
+            # We check if the user's already registered. If not, we create a
+            # visitor user containing only the whitelisted CRSid.
+
+            existing_user = User.query.filter_by(crsid=values["whitelist"]).first()
+            if existing_user:
+                room.whitelisted_users.append(existing_user)
+            else:
+                user = User(
+                    email=None,
+                    full_name=None,
+                    crsid=values["whitelist"],
+                    role=Role.query.filter_by(role=RoleType("visitor")).first(),
+                )
+                db.session.add(user)
+                current_app.logger.info(
+                    f"Registered visitor with CRSid { values['whitelist'] }"
+                )
+                room.whitelisted_users.append(user)
+
+        db.session.commit()
+        flash("Settings saved.")
+        return redirect(url_for("rooms.manage", room_id=room.id))
+    else:
+        flash(_("There were problems with the information you provided."))
+        return render_template(
+            "rooms/manage.html",
+            page_title=f"Settings for { room.name }",
+            room=room,
+            user=user,
+            errors=errors,
+            page_parent=url_for("room_aliases.home", room_id=room.id),
+            **values
+        )
+
+
+@bp.route("/<room_id>/manage", methods=["GET"])
 @auth_decorator
 def manage(room_id):
     # first pass: is the room URL valid?
@@ -89,116 +198,17 @@ def manage(room_id):
         abort(404)
 
     # second pass: does the room belong to the user?
+    # or does it belong to a user's group?
     crsid = auth_decorator.principal
     user = User.query.filter_by(crsid=crsid).first()
-    if room not in user.rooms:
-        abort(403)
-
-    group_id = room.group_id
-    group = Group.query.filter_by(id=group_id).first()
-
-    # ensure that the user belongs to the group they're editing
-
-    if group not in user.groups:
-        abort(403)
-
-    values = {}
-    errors = {}
-
-    if request.method == "POST":
-        keys = (
-            "name",
-            "welcome_text",
-            "banner_text",
-            "banner_color",
-            "authentication",
-            "password",
-            "whitelist",
-            "alias"
-        )
-        values = get_form_values(request, keys)
-
-        for key in ("mute_on_start", "disable_private_chat", "alias_checked"):
-            values[key] = bool(request.form.get(key, False))
-
-        if len(values["welcome_text"]) > 500:
-            errors["welcome_text"] = "Welcome text is too long."
-        if len(values["banner_text"]) > 200:
-            errors["banner_text"] = "Banner text is too long."
-
-        if len(values["whitelist"]) > 7:
-            errors["whitelist"] = "Invalid CRSid."
-
-        if values["alias_checked"]:
-            if values["alias"] == "":
-                errors["alias"] = "You must specify the name of your customised page."
-            elif not validate_room_alias(values["alias"]):
-                errors["alias"] = "Invalid name."
-
-            room_with_alias = Room.query.filter_by(alias=values["alias"]).first()
-
-            if room_with_alias and room_with_alias.id != room.id:
-                errors["alias"] = "That URL is already in use. Choose a different one."
-
-        if not errors:
-            print(Authentication(values["authentication"]))
-
-            room.name = values["name"]
-            room.welcome_text = values["welcome_text"] if values["welcome_text"] != "" else None
-            room.banner_text = values["banner_text"] if values["banner_text"] != "" else None
-            room.banner_color = values["banner_color"]
-            room.authentication = Authentication(values["authentication"])
-            room.alias = values["alias"] if values["alias"] != "" else None
-            room.mute_on_start = values["mute_on_start"]
-            room.disable_private_chat = values["disable_private_chat"]
-            room.updated_at = datetime.now()
-
-            if values["whitelist"] != "":
-
-                current_app.logger.info(
-                    f"{ crsid } is whitelisting '{ values['whitelist'] }' for room '{ group.id }/{ room.name }'..."
-                )
-
-                # Whitelist a new CRSid.
-                # We check if the user's already registered. If not, we create a
-                # visitor user containing only the whitelisted CRSid.
-
-                existing_user = User.query.filter_by(crsid=values["whitelist"]).first()
-                if existing_user:
-                    room.whitelisted_users.append(existing_user)
-                else:
-                    user = User(
-                        email=None,
-                        full_name=None,
-                        crsid=values["whitelist"],
-                        role=Role.query.filter_by(role=RoleType("visitor")).first(),
-                    )
-                    db.session.add(user)
-                    current_app.logger.info(
-                        f"Registered visitor with CRSid { values['whitelist'] }"
-                    )
-                    db.session.commit()
-
-                    room.whitelisted_users.append(user)
-
-            db.session.commit()
-
-            flash("Settings saved.")
-            return redirect(url_for("rooms.manage", room_id=room.id))
-
-        else:
-            flash(_("There were problems with the information you provided."))
-            return render_template(
-                "rooms/manage.html",
-                page_title=f"Settings for { room.name }",
-                room=room,
-                user=user,
-                group=group,
-                errors=errors,
-                page_parent=url_for("groups.manage", group_id=group.id),
-                **values
-            )
-
+    if room.group_id:
+        group_id = room.group_id
+        group = Group.query.filter_by(id=group_id).first()
+        if group not in user.groups:
+            abort(403)
+    else:
+        if room not in user.rooms:
+            abort(403)
 
     # Defaults
     values = {
@@ -216,10 +226,9 @@ def manage(room_id):
         "rooms/manage.html",
         page_title=f"Settings for { room.name }",
         room=room,
-        group=group,
         user=user,
         errors={},
-        page_parent=url_for("groups.manage", group_id=group.id),
+        page_parent=url_for("room_aliases.home", room_id=room.id),
         **values,
     )
 
--- lightbluetent/static/javascripts/rooms.js
+++ lightbluetent/static/javascripts/rooms.js
@@ -7,4 +7,43 @@ $("#nav-tab a").on("shown.bs.tab", function (e) {
 // on load of the page: switch to the currently selected tab
 $(`#nav-tab a[href="${window.sessionStorage.getItem('last-tab')}"]`).tab('show');
 
+$(document).ready(function() {
+
+    /* Show the password div if that's selected on page load */
+    if ($("input[name='authentication']:checked").val() == "password") {
+        $("div.auth-box").hide();
+        $("#auth_box_password").show();
+    }
+
+    /* Show the whitelist div if that's selected on page load */
+    if ($("input[name='authentication']:checked").val() == "whitelist") {
+        $("div.auth-box").hide();
+        $("#auth_box_whitelist").show();
+    }
+
+    /* Show the correct div if the values change */
+    $('input[type="radio"]').change(function() {
+    	var auth_type = $(this).val();
+        $("div.auth-box").hide();
+        $("#auth_box_"+auth_type).show();
+    });
+
+    /* Enable or disable the alias box */
+    $('input[type="checkbox"]').click(function() {
+
+        var input = document.getElementById("alias_input");
+
+        if($(this).is(":checked")) {
+            input.disabled = false;
+        } else {
+            input.disabled = true;
+        }
+
+    });
+
+
+});
+
+
+
 
