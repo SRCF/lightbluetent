191b34239ce883caf230b2dba84071729a5a77d4
Matias Silva <mw781@srcf.net>
2020-10-02 08:58:21 +0100

    Don't use JS for server side rendering

--- lightbluetent/static/javascripts/rooms.js
+++ lightbluetent/static/javascripts/rooms.js
@@ -33,14 +33,6 @@ $('#alias_checkbox').click(function () {
     if(!$(this).is(":checked")) aliasInput.val("")
 });
 
-/* Show the recurring options if they're selected on page load */
-var recurring_checkbox = document.getElementById("recurring_checkbox")
-if (recurring_checkbox.checked) {
-    $("#recurring_box").show();
-} else {
-    $("#recurring_box").hide();
-}
-
 /* Enable the alias input if selected on page load */
 var alias_checkbox = document.getElementById("alias_checkbox")
 var alias_input = document.getElementById("alias_input")
